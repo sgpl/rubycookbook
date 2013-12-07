8dced09d6d5da1c94348b6accd5853d1a9a92025
sgpl <sharad45@gmail.com>
2013-12-07 23:01:53 +0300

    ...

--- c1/rough.rb
+++ c1/rough.rb
@@ -1,1 +1,18 @@
 # ch1 examples:
+
+
+string = "My first string"
+puts string.slice(3,5) # => first # 5 char starting at index[3]
+puts string[3,5] # also has the same output
+
+
+#mutability : can change after declaration: 
+
+puts string.upcase
+puts string
+puts string.upcase!
+puts string
+
+puts string.empty?
+puts string.include? 'my' # because we changed string above
+puts string.include? 'MY'
