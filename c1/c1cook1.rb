5b4d9eba1265ff9efd4fd5954a51c522d1df8a7d
sgpl <sharad45@gmail.com>
2013-12-10 15:04:33 +0300

    added stuff to file

--- c1/c1cook1.rb
+++ c1/c1cook1.rb
@@ -1,9 +1,10 @@
 # building string from parts:
 
-str1 = ['h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd']
+str1 = ["h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d"]
 
-# for i in str1
-# 	print str1[i]
-# end
+for i in str1
+	print i
+end
+
 
 str1.each {|n| print n; sleep 1}
