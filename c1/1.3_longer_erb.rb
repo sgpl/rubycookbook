407384b393dd1f5a8ed32f5f40a8f1034f6d26bb
sgpl <sharad45@gmail.com>
2014-01-19 23:35:18 +0300

    added stuff to the recepie

--- c1/1.3_longer_erb.rb
+++ c1/1.3_longer_erb.rb
@@ -21,3 +21,18 @@ puts template.run(binding)
 problems = []
 puts template.run(binding)
 
+################## different part ###########
+class String
+	def substitute(binding=TOPLEVEL_BINDING)
+		eval(%{"#{self}"},binding)
+	end
+end
+
+template_new = %q{ Chunky #{food}! }
+
+food = 'bacon'
+puts template_new.substitute(binding)
+food = 'peanut butter'
+puts template_new.substitute(binding)
+
+# refer to notes on page 9. of ruby cookbook for imp. notes. 
