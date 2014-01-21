318a70c336d28aceab6e38665ee06fd3d3e939c7
sgpl <sharad45@gmail.com>
2014-01-21 23:37:52 +0300

    more work on the unprintable characters from recipe 1.5

--- c1/1.5_unprintable_characters.rb
+++ c1/1.5_unprintable_characters.rb
@@ -1,2 +1,16 @@
 octal = "\000\001\010\020" 
 octal.each_byte { |x| puts x }
+# 0 
+# 1
+# 8
+# 16
+
+# can reference any binary character by encoding its octal representation like "\000"
+# can reference hexadecimal representation such as "\x00"
+
+hexadecimal = "\x00\x01\x10\x20" 
+hexadecimal.each_byte { |x| puts x } 
+# 0
+# 1
+# 16
+# 32
---
+++ c1/2rough.rb
@@ -0,0 +1,6 @@
+open('smiley.html', "wb") do |f| 
+	f << '<meta http-equiv="Content-Type" content="text/html;charset=UTF-8"'
+	f << "\xe2\x98\xBA"
+end
+
+# generates a smiley.html webpage
